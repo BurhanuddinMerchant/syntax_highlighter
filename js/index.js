6660b9700a387eca4993af8764f16440d1f6aae4
BURHANUDDIN <Bmerchant945@gmail.com>
2020-12-26 11:46:21 +0530

    added the c++ keywords in an array

--- js/index.js
+++ js/index.js
@@ -3,3 +3,129 @@ const output = document.getElementById("output");
 const updateOutput = () => {
   output.innerHTML = input.value;
 };
+const isCppKeyword = (word) => {
+  switch (word) {
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+    case "": {
+      break;
+    }
+  }
+};
+const cppkeywords = [
+  "asm",
+  "else",
+  "new",
+  "this",
+  "auto",
+  "enum",
+  "operator",
+  "throw",
+  "bool",
+  "explicit",
+  "private",
+  "true",
+  "break",
+  "export",
+  "protected",
+  "try",
+  "case",
+  "extern",
+  "public",
+  "typedef",
+  "catch",
+  "false",
+  "register",
+  "typeid",
+  "char",
+  "float",
+  "reinterpret_cast",
+  "typename",
+  "class",
+  "for",
+  "return",
+  "union",
+  "const",
+  "friend",
+  "short",
+  "unsigned",
+  "const_cast",
+  "goto",
+  "signed",
+  "using",
+  "continue",
+  "if",
+  "sizeof",
+  "virtual",
+  "default",
+  "inline",
+  "static",
+  "void",
+  "delete",
+  "int",
+  "static_cast",
+  "volatile",
+  "do",
+  "long",
+  "struct",
+  "wchar_t",
+  "double",
+  "mutable",
+  "switch",
+  "while",
+  "dynamic_cast",
+  "namespace",
+  "template",
+];
